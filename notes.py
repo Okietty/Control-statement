603cb10d4c11ad02be02e6552026a9d5ab406a19
Kiet Tran <kiettran0816@gmail.com>
2022-07-14 13:34:21 -0400

    if-else notes

--- notes.py
+++ notes.py
@@ -1,1 +1,36 @@
+#user = input("Type in a word: ")
+#while user != "quit":
+#    print(user.capitalize())
+#    user = input("Type in a word: ")
 
+
+
+#for i in range(26):
+#    print(i)
+
+
+
+#numOfLoop = int(input("How many times should this loop for: "))
+#allNums = 0
+#for i in range(numOfLoop):
+#    askForNum = int(input("Type in a number: "))
+#    allNums += askForNum
+
+#print("The average is: ", allNums / numOfLoop)
+
+#sum_num = int(input("Type in a number: "))
+
+#while sum_num < 300:
+#    askForNumbers = int(input("Type in a number: "))
+#    sum_num += askForNumbers
+
+#print("The sum is now bigger than 300")
+
+for i in range(-10, 21, 1):
+    print('i =', i)
+
+for i in range(-100, -45, 5):
+    print('i =', i)
+
+for i in range(-10, -21, -2):
+    print('i =', i)
